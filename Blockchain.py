02c500a9b41a25f8392134af4b4ed6f214769332
arju88nair <arju88nair@gmail.com>
2018-03-22 16:12:21 +0530

    Genesis block and hash

--- Blockchain.py
+++ Blockchain.py
@@ -1,11 +1,41 @@
+import hashlib
+import json
+from time import time
+
+
 class Blockchain(object):
     def __init__(self):
+        self.current_transactions = []
         self.chain = []
+
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new Block in the Blockchain
+
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+
+        # When our Blockchain is instantiated we’ll need to seed it with a genesis block—a block with no predecessors.
+        #  We’ll also need to add a “proof” to our genesis block which is the result of mining (or proof of work).
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transactions
         self.current_transactions = []
 
-    def new_block(self):
-        # Creates a new Block and adds it to the chain
-        pass
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         """
@@ -17,6 +47,10 @@ class Blockchain(object):
         :return: <int> The index of the Block that will hold this transaction
         """
 
+        # After new_transaction() adds a transaction to the list, it returns the index of the block which
+        # the transaction will be added to—the next one to be mined. This will be useful later on,
+        # to the user submitting the transaction.
+
         self.current_transactions.append({
             'sender': sender,
             'recipient': recipient,
@@ -25,12 +59,19 @@ class Blockchain(object):
 
         return self.last_block['index'] + 1
 
-    @staticmethod
-    def hash(block):
-        # Hashes a Block
-        pass
-
     @property
     def last_block(self):
-        # Returns the last Block in the chain
-        pass
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        """
+        Creates a SHA-256 hash of a Block
+
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
